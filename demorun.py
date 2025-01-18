18288bfd02152857e1b663c0cdba9c6bed235e19
YixinChen-AI <yixinchen970320@126.com>
2025-01-18 13:01:17 +0800

    fixbug

--- demorun.py
+++ demorun.py
@@ -3,27 +3,22 @@ import sys,os,json
 import SimpleITK as sitk
 import numpy as np
 import torch.nn as nn
-sys.path.append("/share/home/yxchen/github/MPUM")
+sys.path.append(".")
 from datautils import resampleVolume,adjust_image_direction,resample_image
 from inference import inference
-
-
 from tqdm import tqdm
 import pandas as pd
-os.environ["CUDA_VISIBLE_DEVICES"]='7'
-
 import sys
-sys.path.append("/share/home/yxchen/github/MPUM")
 from inference import inference
 config = {
                 "tissue":"all",
                 "modality":"ct",
                 "modelsize":"base",
                 "modalitydimension":512,
-                "ckpt":["/share/home/yxchen/08TripleUniversalModel/model_weight/3fold_MPUM_base_512/fold1/epoch_43.pth",
-                        "/share/home/yxchen/08TripleUniversalModel/model_weight/3fold_MPUM_base_512/fold2/epoch_42.pth",
-                        "/share/home/yxchen/08TripleUniversalModel/model_weight/3fold_MPUM_base_512/fold0/epoch_40.pth"]
+                "ckpt":["./weights/fold0.pth",
+                        "./weights/fold1.pth",
+                        "./weights/fold2.pth",]
             }
 inference(config,
-         nii_path="/share/home/yxchen/github/MPUM/samples/sample_pet.nii.gz",
-         output_seg_path="/share/home/yxchen/github/MPUM/samples/output_pet")
+         nii_path="./samples/sample_pet.nii.gz",
+         output_seg_path="./samples/output_pet_demo")
